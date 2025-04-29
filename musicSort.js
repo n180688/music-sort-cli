e56ded9c19db51fcb3f8a85a2a1e0f22256a160c
Grbrbr <150646071+n180688@users.noreply.github.com>
2025-04-29 17:18:30 +0300

    +проверка по метаданным песни

--- musicSort.js
+++ musicSort.js
@@ -23,40 +23,63 @@ if (!filterWords) {
 // Приводим путь к абсолютному
 const musicPath = path.resolve(inputPath);
 
+
 //считывание метаданных
 async function mm(filePath) {
   try {
     const metadata = await parseFile(filePath);
+return {
+	title: metadata.common.title,
+	artist: metadata.common.artist,
+	path: filePath,
+	}
 
-console.log(`Title: ${metadata.common.title}, Artist: ${metadata.common.artist}`);  
 } catch (error) {
-    console.error('Error parsing metadata:', error.message);
+   	return{
+		path: filePath,
+		error: error.message,
+	}
+
   }
 };
 
 
 
+
 async function main() {
     try {
+	
         const files = await fs.readdir(musicPath);
-
-        // Фильтрация по имени файла
-	const filteredFiles = files.filter(file => 
-  filterWords.some(word => file.toLowerCase().includes(word.toLowerCase()))
-); 
 
 
         console.log(`Всего файлов найдено: ${files.length}`);
-        console.log(`Файлов, содержащих "${filterWords}": ${filteredFiles.length}`);
-	
 
-//filteredFiles.forEach((file) => {console.log(file); mm(path.join(musicPath, file))});
+//получение метаданных из всех песен
+const mdPromises = files.map(file => mm(path.join(musicPath, file)));
 
+const allMetadata = await Promise.all(mdPromises);
+
+//фильтрация
+const filteredSongs = allMetadata.filter(file => {
+	const lowerTitle = file.title?.toLowerCase() || '';
+	const lowerArtist = file.artist?.toLowerCase() || '';
+	const lowerFilename = path.basename(file.path).toLowerCase();
+
+	const hasWordInTitle = filterWords.some(word => lowerTitle.includes(word.toLowerCase()));
+	const hasWordInArtist = filterWords.some(word => lowerArtist.includes(word.toLowerCase()));
+	const hasWordInFilename = filterWords.some(word => lowerFilename.includes(word.toLowerCase()));
+
+return hasWordInTitle || hasWordInArtist || hasWordInFilename;
+
+});
+
+console.log(`Файлов, содержащих "${filterWords}": ${filteredSongs.length}`);
+console.log(filteredSongs);
 
 
     } catch (error) {
         console.error(`Ошибка при чтении папки: ${error.message}`);
-    }
+  }
 }
 
 
